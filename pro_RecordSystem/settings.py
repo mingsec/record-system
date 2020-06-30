f196bf609794ae314bcfe823fe61fe4d51ed8d03
mingsec <mingsec@outlook.com>
2020-06-30 13:36:43 +0800

    version0.2.11，详见ReadMe

--- pro_RecordSystem/settings.py
+++ pro_RecordSystem/settings.py
@@ -169,5 +169,5 @@ if os.getcwd() == '/app':
 
     # 静态资产配置
     BASE_DIR         = os.path.dirname(os.path.abspath(__file__))
-    STATIC_ROOT      = 'static'
+    STATIC_ROOT      = 'staticfiles'
     STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
